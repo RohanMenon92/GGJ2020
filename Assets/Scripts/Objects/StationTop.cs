cb76729cb2e4d3495e498c0e75c22582cd4d62ab
Thanik <thanik@gmail.com>
2020-02-01 06:27:38 +0000

    Player can cancel mid-way of working on station & add minigames folder

--- Assets/Scripts/Objects/StationTop.cs
+++ Assets/Scripts/Objects/StationTop.cs
@@ -26,22 +26,72 @@ public class StationTop : MonoBehaviour
         }
     }
 
-    public void interact(PlayerBot player, ProductBox box)
+    public void Interact(PlayerBot player, ProductBox box)
     {
-        if (box)
+        if (box && !currentBox)
         {
             currentBox = box;
-            if (box.doneIndex < box.processes.Count && box.processes[box.doneIndex] == stationType)
+            player.carryingBox = null;
+            if (box.doneIndex < box.processes.Count && box.processes[box.doneIndex] == stationType && PlayerTypeCompatibleCheck(player))
             {
                 isWorking = true;
                 progress = 0f;
             }
 
-            if (!isWorking)
+            if (!isWorking && progress >= 1f)
             {
                 player.carryingBox = currentBox;
                 currentBox = null;
             }
+        }
+        else if(currentBox)
+        {
+            isWorking = true;
+        }
+    }
+
+    public void Cancel()
+    {
+        isWorking = false;
+    }
+
+    private bool PlayerTypeCompatibleCheck(PlayerBot player)
+    {
+        if (player.botType == GameConstants.PlayerBotType.Heavy)
+        {
+            switch(stationType)
+            {
+                case GameConstants.StationType.HBuffing:
+                case GameConstants.StationType.HDisassembly:
+                case GameConstants.StationType.HWiring:
+                case GameConstants.StationType.CInspection:
+                case GameConstants.StationType.CPolishing:
+                case GameConstants.StationType.CTable:
+                    return true;
+                default:
+                    return false;
+
+            }
+        }
+        else if(player.botType == GameConstants.PlayerBotType.Light)
+        {
+            switch (stationType)
+            {
+                case GameConstants.StationType.LElectric:
+                case GameConstants.StationType.LProgramming:
+                case GameConstants.StationType.LWelding:
+                case GameConstants.StationType.CInspection:
+                case GameConstants.StationType.CPolishing:
+                case GameConstants.StationType.CTable:
+                    return true;
+                default:
+                    return false;
+
+            }
+        }
+        else
+        {
+            return false;
         }
     }
 }
