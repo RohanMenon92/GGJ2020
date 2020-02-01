8e074709cd24b36a4a79ed4e2b8ffffbbf5a4996
Thanik <thanik@gmail.com>
2020-02-01 14:07:02 +0000

    Added TextMeshPro

--- Assets/Scripts/UIElements/Score.cs
+++ Assets/Scripts/UIElements/Score.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
+    private TMP_Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText = GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
--- Assets/Scripts/UIElements/Timer.cs
+++ Assets/Scripts/UIElements/Timer.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Timer : MonoBehaviour
 {
+    private TMP_Text timerText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timerText = GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
