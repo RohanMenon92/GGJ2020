cb71fa1d5dd8680bdff128e7ea1f175522bac86b
ROHAN MENON <1901120@uad.ac.uk>
2020-02-01 03:41:03 +0000

    Added more classes

---
+++ Assets/Scripts/Objects/ProductBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductBox : MonoBehaviour
+{
+    public int boxID;
+    public List<GameConstants.StationType> processes;
+    public int doneIndex;
+    public bool inspected;
+    public float timeToRepair;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
