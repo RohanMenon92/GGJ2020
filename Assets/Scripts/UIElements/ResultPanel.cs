8edb729b796350ddc21c7b86a6b9c6f97775de4d
Thanik <thanik@gmail.com>
2020-02-01 15:38:32 +0000

    Added Ready before game start text, some draft UI, add post-process stack (just in case)

---
+++ Assets/Scripts/UIElements/ResultPanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ResultPanel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TMP_Text boxScore;
+    public TMP_Text timeBonusScore;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
