186ca33d789c7f5dcf7dc57f295475a730f46fd4
ROHAN MENON <1901120@uad.ac.uk>
2020-02-01 04:04:45 +0000

    Added Scene Changes

--- Assets/Scripts/UserControl.cs
+++ Assets/Scripts/UserControl.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class UserControl : MonoBehaviour
 {
+    bool UpPressed, DownPressed, LeftPressed, RightPressed, InteractPressed;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    void ButtonPressed(string message) {
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(UpPressed)
+        {
+            
+        } 
         
+
     }
 }
