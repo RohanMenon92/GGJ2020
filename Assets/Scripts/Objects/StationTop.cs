15d3e11b1a9cd92328b15443652fab56debe0f57
Thanik <thanik@gmail.com>
2020-02-02 08:37:32 +0000

    Added progressbar, move UI prefabs into a separate folder

--- Assets/Scripts/Objects/StationTop.cs
+++ Assets/Scripts/Objects/StationTop.cs
@@ -10,26 +10,62 @@ public class StationTop : MonoBehaviour
     public float speed;
     public ProductBox currentBox;
     public Transform boxTransform;
+
+    public GameObject progressBarPrefab;
+    private StationProgressBar progressBar;
+    private Canvas canvas;
+    private RectTransform canvasRect;
+    public float progressBarOffsetZ;
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvas = FindObjectOfType<Canvas>();
+        canvasRect = canvas.GetComponent<RectTransform>();
+        GameObject progressBarGameObject = Instantiate(progressBarPrefab, canvas.transform);
+        progressBar = progressBarGameObject.GetComponent<StationProgressBar>();
+        // Final position of marker above GO in world space
+        Vector3 offsetPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + progressBarOffsetZ);
+
+        // Calculate *screen* position (note, not a canvas/recttransform position)
+        Vector2 canvasPos;
+        Vector2 screenPoint = Camera.main.WorldToScreenPoint(offsetPos);
+
+        // Convert screen position to Canvas / RectTransform space <- leave camera null if Screen Space Overlay
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, screenPoint, null, out canvasPos);
+
+        // Set
+        progressBar.transform.localPosition = canvasPos;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (isWorking)
         {
+            
             progress += speed * Time.deltaTime;
+            if (progressBar)
+            {
+                progressBar.setStatusBarMode(ProgressBarMode.WITHBG);
+                progressBar.updateBar(progress, 1f);
+            }
             if (progress >= 1f)
             {
                 currentBox.ProcessDone(stationType);
                 isWorking = false;
             }
         }
+        else
+        {
+            // so the progressbar will show only when isWorking is true
+            if (progressBar)
+            {
+                progressBar.setStatusBarMode(ProgressBarMode.HIDDEN);
+            }
+        }
 
-        if(currentBox)
+        if (currentBox)
         {
             // make the box place on the top of station
             currentBox.transform.position = boxTransform.position;
--- Assets/Scripts/UIElements/StationProgressBar.cs
+++ Assets/Scripts/UIElements/StationProgressBar.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum ProgressBarMode
+{
+    HIDDEN,
+    NOBG,
+    WITHBG
+}
 
 public class StationProgressBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ProgressBarMode mode = ProgressBarMode.HIDDEN;
+
+    public Image healthBar;
+    public Image background;
+    public void updateBar(float value, float maxValue)
     {
-        
+        healthBar.fillAmount = (float)value / (float)maxValue;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void setStatusBarMode(ProgressBarMode newMode)
     {
-        
+        mode = newMode;
+        if (mode == ProgressBarMode.HIDDEN)
+        {
+            var tempColor = healthBar.color;
+            tempColor.a = 0f;
+            healthBar.color = tempColor;
+
+            tempColor = background.color;
+            tempColor.a = 0f;
+            background.color = tempColor;
+        }
+        else if (mode == ProgressBarMode.NOBG)
+        {
+            var tempColor = healthBar.color;
+            tempColor.a = 0.65f;
+            healthBar.color = tempColor;
+
+            tempColor = background.color;
+            tempColor.a = 0f;
+            background.color = tempColor;
+        }
+        else if (mode == ProgressBarMode.WITHBG)
+        {
+            var tempColor = healthBar.color;
+            tempColor.a = 0.8f;
+            healthBar.color = tempColor;
+
+            tempColor = background.color;
+            tempColor.a = 0.8f;
+            background.color = tempColor;
+        }
     }
 }
